40dd849fd1bbadaa0c45d65f72592be814467945
siar <siarmehri.angel@gmail.com>
2018-03-24 21:03:15 +0000

    homestead mysql username's default password is secret.

--- config/database.php
+++ config/database.php
@@ -58,7 +58,7 @@ return [
             'port'      => env('DB_PORT', '3306'),
             'database'  => env('DB_DATABASE', 'homestead'),
             'username'  => env('DB_USERNAME', 'homestead'),
-            'password'  => env('DB_PASSWORD', 'homestead'),
+            'password'  => env('DB_PASSWORD', 'secret'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => env('DB_PREFIX', ''),

efc375a4b984c99bb10e8631dd67d4bbabce7e4d
siar <siarmehri.angel@gmail.com>
2018-03-26 01:08:33 +0100

    Seed table from floatrates.com

--- addons/default/madebysauce/convertcurrencies-module/src/Convertcurrency/ConvertcurrencySeeder.php
+++ addons/default/madebysauce/convertcurrencies-module/src/Convertcurrency/ConvertcurrencySeeder.php
@@ -24,11 +24,21 @@ class ConvertcurrencySeeder extends Seeder
      */
     public function run()
     {
-        dump('Here I Will Be Reading The http://www.floatrates.com/daily/'.$this->currency.'.xml] file and will seed my table');
-
         $this->convertcurrencyRepository->truncate();
+        dump('Here I Will Be Reading The http://www.floatrates.com/daily/'.$this->currency.'.xml file and will seed my table');
+        $url = 'http://www.floatrates.com/daily/'.$this->currency.'.xml';
 
+        $currencyRates = json_decode(json_encode((array) simplexml_load_file($url)), 1);
 
-
+        foreach ($currencyRates['item'] as $currencyRate){
+            $this->convertcurrencyRepository->create(array(
+                'pubDate' => $currencyRate['pubDate'],
+                'baseCurrency' => $currencyRate['baseCurrency'],
+                'baseName' => $currencyRate['baseName'],
+                'targetCurrency' => $currencyRate['targetCurrency'],
+                'targetName' => $currencyRate['targetName'],
+                'exchangeRate' => round($currencyRate['exchangeRate'], 2)
+            ));
+        }
     }
 }
